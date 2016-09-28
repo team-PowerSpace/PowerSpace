251c7cd59d496345f6cd784867a513ed16cce71b
Andrew Sautin <sautin1@yandex.ru>
2016-09-28 16:46:04 +0300

    Minor mistakes and style corrected

--- PowerSpace/Common/Script.h
+++ PowerSpace/Common/Script.h
@@ -3,7 +3,7 @@
 class IScript
 {
 public:
-	virtual ~IScript() {}
-	virtual void OnClick() = 0;
-	virtual void OnTimer() = 0;
+    virtual ~IScript() {}
+    virtual void OnClick() = 0;
+    virtual void OnTimer() = 0;
 };
