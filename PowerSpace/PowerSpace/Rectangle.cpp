73e29e92c1d50bdbbc496bb9fe079cad2dbc1ca4
Lebedev Aleksey <leb-al@outlook.com>
2016-10-07 18:57:44 +0300

    Remove old useless files (Rectangle.*)

--- PowerSpace/PowerSpace/Rectangle.cpp
+++
@@ -1,7 +0,0 @@
-#include "Rectangle.h"
-
-
-void Rectangle::Draw( HDC paintDc )
-{
-	
-}
--- PowerSpace/PowerSpace/Rectangle.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "Drawable.h"
-
-class Rectangle : public IDrawable {
-public:
-	virtual void Draw( HDC paintDc ) override;
-};
