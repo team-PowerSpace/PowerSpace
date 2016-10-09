1709bddf798696b6d11b2398df654e6a5e3757e7
Krivchanskii Nikolay <krivch.nik@gmail.com>
2016-10-09 19:20:18 +0300

    Finalized script engine wrapper

--- PowerSpace/ScriptEngine/ScriptEngine.cpp
+++ PowerSpace/ScriptEngine/ScriptEngine.cpp
@@ -1,15 +1,46 @@
 #include "../stdafx.h"
 #include "ScriptEngine.h"
-
+#include "ScriptSolver.h"
+#include "PyObjectBuilder.h"
+#include "CDrawableBuilder.h"
+#include <codecvt>
 
 CScriptEngine::CScriptEngine( CStage& _stage )
     : stage( _stage )
 {}
 
-std::vector<int> CScriptEngine::RunScripts( int objectId, const std::vector<CScript>& scripts )
+void CScriptEngine::setNewValues(CDrawableBuilder::CDrawableStruct& newData, std::shared_ptr<IDrawable> workingObject)
 {
-    // TODO: implement this method
-    UNREFERENCED_PARAMETER( objectId );
-    UNREFERENCED_PARAMETER( scripts );
-    return std::vector<int>();
+	workingObject->SetColor(newData.color);
+	TPosition rect;
+	rect.left = newData.xPos;
+	rect.top = newData.yPos;
+	rect.right = newData.xPos + newData.width;
+	rect.bottom = newData.yPos + newData.height;
+	workingObject->SetPosition(rect);
+}
+
+std::vector<int> CScriptEngine::RunScripts(int objectId, const std::vector<CScript>& scripts )
+{
+	std::shared_ptr<IDrawable> workingObject = stage.GetObjectById(objectId);
+	CPyObjectBuilder builder(workingObject);
+	std::shared_ptr<PyObject> pyObject = builder.GetpObject();
+
+	using convert_type = std::codecvt_utf8<wchar_t>;
+	std::wstring_convert<convert_type, wchar_t> converter; //to convert from TPath (std::wstring) to std::string
+
+	std::string emptyString = "";
+
+	for (auto i = scripts.begin(); i != scripts.end(); i++)
+	{
+		TPath wstrPath((*i).GetPath());
+		std::string strPath = converter.to_bytes(wstrPath);
+		CScriptSolver solver(pyObject, strPath, emptyString); //Empty string left for ability to call different functions located in single script
+		std::shared_ptr<PyObject> changedObject = solver.Run();
+		CDrawableBuilder::CDrawableStruct newData;
+		assert( (PyArg_ParseTuple(changedObject.get(), "k", newData.color, //Check that extracted data corresponds correctly with types
+			"i" , newData.xPos, "i" , newData.yPos, "i", newData.width, "i", newData.height)) > 0);
+		setNewValues(newData, workingObject);
+	}
+    return std::vector<int>(); //Not used for now, but later will allow to return list of objects changed (if needed) for 
 }
--- PowerSpace/ScriptEngine/ScriptEngine.h
+++ PowerSpace/ScriptEngine/ScriptEngine.h
@@ -12,5 +12,6 @@ public:
     // returns the vector of objects that were modified during the evaluation of the scripts
     std::vector<int> RunScripts( int objectId, const std::vector<CScript>& scripts );
 private:
+	void setNewValues(CDrawableBuilder::CDrawableStruct& newData, std::shared_ptr<IDrawable> workingObject);
     CStage& stage;
 };
