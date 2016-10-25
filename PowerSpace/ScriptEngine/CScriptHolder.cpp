53729ff2914d133dc84aa3d804a83c477891d0fe
ZatsepinMikhail <salamence1996@gmail.com>
2016-10-25 12:45:37 +0300

    remove duplicate CScriptHolder files

--- PowerSpace/ScriptEngine/CScriptHolder.cpp
+++ PowerSpace/ScriptEngine/CScriptHolder.cpp
@@ -1,31 +1,46 @@
+#include <stdafx.h>
+
 #include "CScriptHolder.h"
 
-bool ScriptHolder::addScript(std::wstring path, PyObject* script)
+bool ScriptHolder::addScript( std::wstring path, PyObject* script )
 {
-	std::pair<std::wstring, PyObject*> toInsert(path, script);  //For now scripts hotfixes in running Viewer are not supported
-	return (Scripts.insert(toInsert)).second;					//Returns true if was inserted succefully
+	std::pair<std::wstring, PyObject*> toInsert( path, script );  //For now scripts hotfixes in running Viewer are not supported
+	Py_INCREF( script );
+	return (Scripts.insert( toInsert )).second;					//Returns true if was inserted succefully
 }
 
-PyObject* ScriptHolder::getScript(std::wstring path)
+int ScriptHolder::getSize() const
+{
+	return Scripts.size();
+}
+
+PyObject* ScriptHolder::getScript( std::wstring path )
 {
 	PyObject* obj;
-	try
-	{
-		obj = Scripts.at(path);		//Is okay if operation succeful
-	}
-	catch (std::out_of_range e)
-	{
+	try {
+		obj = Scripts.at( path );		//Is okay if operation succeful
+	} catch( std::out_of_range e ) {
 		return nullptr;
 	}
+	Py_INCREF( obj );
 	return obj;
 }
 
-bool ScriptHolder::isScriptIn(std::wstring path)
+bool ScriptHolder::isScriptIn( std::wstring path )
 {
-	auto iterator = Scripts.find(path);
-	if (iterator == Scripts.end())
-	{
+	auto iterator = Scripts.find( path );
+	if( iterator == Scripts.end() ) {
 		return false;		 //If points to the .end() then gg
 	}
 	return true;			 //If you want to support hotfixes to scripts -- don't forget to update code here
 }
+
+//TODO: Need to solve link error with scriptEngine.obj
+/*
+ScriptHolder::~ScriptHolder()
+{
+	for (auto iterator : Scripts) {
+	Py_DECREF(iterator.second);
+	}
+}
+*/
--- PowerSpace/ScriptEngine/CScriptHolder.h
+++ PowerSpace/ScriptEngine/CScriptHolder.h
@@ -2,11 +2,16 @@
 #include <Python.h>
 #include <unordered_map>
 
-class ScriptHolder {
+class ScriptHolder
+{
 	std::unordered_map<std::wstring, PyObject*> Scripts; //Scripts, that were run on this session
 
 public:
-	bool isScriptIn(std::wstring path);					 //Check is script in
-	PyObject* getScript(std::wstring path);				 //Get script by path
-	bool addScript(std::wstring path, PyObject* script); //Add script that is not into the Holder yet
+	bool isScriptIn( std::wstring path );					 //Check is script in
+	PyObject* getScript( std::wstring path );				 //Get script by path
+	bool addScript( std::wstring path, PyObject* script );   //Add script that is not into the Holder yet
+	int getSize() const;
+
+	//TODO: Is needed to solve link error 2005 with script engine.obj
+	//~ScriptHolder();                                         //We need to free refs to modules
 };
